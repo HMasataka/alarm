bbe68dcb2b5ee7ec37eee74f7d9c2acfd5e9c644
Masataka <masataka.hisasue@knocknote.co.jp>
2022-03-26 20:28:46 +0900

    Feat add control

---
+++ Alarm/FormSet.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Alarm
+{
+    partial class FormSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonAlarm = new System.Windows.Forms.RadioButton();
+            this.radioButtonTimer = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButtonAlarm
+            // 
+            this.radioButtonAlarm.AutoSize = true;
+            this.radioButtonAlarm.Location = new System.Drawing.Point(164, 58);
+            this.radioButtonAlarm.Name = "radioButtonAlarm";
+            this.radioButtonAlarm.Size = new System.Drawing.Size(84, 29);
+            this.radioButtonAlarm.TabIndex = 0;
+            this.radioButtonAlarm.TabStop = true;
+            this.radioButtonAlarm.Text = "Alarm";
+            this.radioButtonAlarm.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTimer
+            // 
+            this.radioButtonTimer.AutoSize = true;
+            this.radioButtonTimer.Location = new System.Drawing.Point(434, 58);
+            this.radioButtonTimer.Name = "radioButtonTimer";
+            this.radioButtonTimer.Size = new System.Drawing.Size(81, 29);
+            this.radioButtonTimer.TabIndex = 1;
+            this.radioButtonTimer.TabStop = true;
+            this.radioButtonTimer.Text = "Timer";
+            this.radioButtonTimer.UseVisualStyleBackColor = true;
+            this.radioButtonTimer.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(164, 122);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 31);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(164, 207);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(180, 31);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(423, 122);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(180, 31);
+            this.numericUpDown3.TabIndex = 4;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(423, 207);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(180, 31);
+            this.numericUpDown4.TabIndex = 5;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(164, 330);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(112, 34);
+            this.buttonOK.TabIndex = 6;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(423, 330);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(112, 34);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // FormSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.radioButtonTimer);
+            this.Controls.Add(this.radioButtonAlarm);
+            this.Name = "FormSet";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonAlarm;
+        private System.Windows.Forms.RadioButton radioButtonTimer;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
--- Alarm/FormSet.cs
+++ Alarm/FormSet.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Alarm
 {
-    public partial class Form1 : Form
+    public partial class FormSet : Form
     {
-        public Form1()
+        public FormSet()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
         }
